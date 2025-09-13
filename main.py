6dd616c219975da3e47ff850ec1cb23c4bfa0c4a
Aarish khan <khan.aarish2346@gmail.com>
2025-09-14 01:00:05 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as p
+
+
+data= p.DataFrame({
+"play type":["run","pass","run","pass","pass"],
+"down":[1,2,3,4,1],
+"yards to go":[10,5,7,3,8],
+"yards gained":[7,3,5,2,9],
+"passer":["tom brady","harry","ash","serena","shogun"],
+"reciver":["kaneki","rintaro","suga","hinata","nishinoya"],
+"rusher":["naruto","sauske","itachi","madara","obito"],
+"result":["first down","first down","incomplete","incomplete","touchdown"]
+})
+# sns.barplot(x="play type",y="yards gained",data=data,palette="bwr")
+# plt.title("FootBall")
+# sns.set_context("poster",font_scale=2)
+# sns.set_style("darkgrid",)
+# plt.grid()
+# plt.figure(figsize=(11,11))
+# sns.pairplot(data)
+# plt.show()
+# player_data=data.groupby("passer")["yards gained"].sum()
+# colors = ['#4B9CD3', '#28527A', '#8AC6D1', '#9AD3BC', '#BFD8B8']
+# passchart=sns.barplot(x=player_data.index,y=player_data.values,palette=colors)
+# plt.xticks(rotation=45)
+# plt.show()
+sns.pairplot(data,hue="play type",palette="bwr")
+plt.show()
